47b5de4f511dec697e1a00516ac005d9762c9b5d
Maria Teresa Rojo <mariatrojo@gmail.com>
2019-10-15 16:44:10 -0700

    detectCapitalUse

---
+++ detectCapitalUse.js
@@ -0,0 +1,12 @@
+var detectCapitalUse = function(word) {
+    if (word.toUpperCase() === word){
+        return true;
+    }
+    if (word.toLowerCase() === word){
+        return true;
+    }
+    if (word[0].toUpperCase() + word.substr(1).toLowerCase() === word){
+        return true;
+    }
+    return false;
+};
