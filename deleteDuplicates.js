d143cf26b86d1a9b8654ead635e8419fe86a5b9a
Maria Teresa Rojo <mariatrojo@gmail.com>
2019-11-08 10:40:24 -0800

    deleteDuplicates

---
+++ deleteDuplicates.js
@@ -0,0 +1,13 @@
+var deleteDuplicates = function(head) {
+    var current = head;
+    var num = head;
+    
+    while (current != null && current.next != null){
+        if (current.val === current.next.val){
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
